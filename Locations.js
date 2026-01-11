a6f42c858f87e99c7d43903d0d7ddddb9318c405
PeckhamPasteEater <27rchen@pinewood.edu>
2026-01-11 15:27:24 -0800

    Update Locations.js

--- Locations.js
+++ Locations.js
@@ -1,18 +1,18 @@
-const tourStops = [
+const locations = [
   {
-    id: 1,
+    id: "duvenecks_arrival",
     title: "The Duvenecks Arrive in the Bay Area",
     lat: 37.44472,
     lng: -122.15298,
     description: "The Duvenecks had visited and, as such, been enchanted by California in their travels around the world. However, it wasn’t until a particularly harsh winter in their native Boston that they decided to officially light out for the West. As World War I raged, they settled in temperate Santa Barbara for a short period, only to move to Carmel and later Tacoma, Washington on account of Frank’s enlisting as an engineer in the military. Soon, Frank was sent to the European theatre of war, but not before ensuring Josephine learned how to drive a Ford Model T. Eight months pregnant with her third child, Josephine brought the family to Palo Alto in 1918, then and still a university town, albeit then one of under 5,000 residents. Josephine rented a house here, at the crook of Addison and Cowper.",
-        radius: 50 // meters before it activates
+   // image  
   },
   {
-    id: 2,
+    id: "duvenecks_return",
     title: "Frank and Josephine return”,
     lat: 37.44855,
     lng: -122.14005,
     description: "Frank was only away for one year. Much had changed; after giving birth to Hope in Palo Alto, Josephine had also gone away to care for Frank’s father in Cincinnati, Ohio, who had developed cancer due to a lifetime of smoking. When Josephine returned, she bought a house here on Newell Road for a sum total of $7,500. What is now South Bay suburban sprawl was once the city’s limit. The Duvenecks, standing here, would be perched on the edge of human settlement. The year was 1919; as a member of the occupying force of Germany, Frank remained after the armistice until August. When he returned, he saw his daughter for the first time.",
-        radius: 50
+    //image
   }
 ];
