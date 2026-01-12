02610b7f3e7477d086d8c8c9e508abe682940239
PeckhamPasteEater <27rchen@pinewood.edu>
2026-01-11 16:42:46 -0800

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("notificationclick", event => {
-  const locationId = event.notification.data;
-  event.notification.close();
-
-  event.waitUntil(
-    clients.openWindow(`index.html#${locationId}`)
-  );
-});
